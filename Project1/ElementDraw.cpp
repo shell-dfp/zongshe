01398666faa8557b9814fe55096fbe6d0af6be46
Shell <1574138381@qq.com>
2025-10-11 17:27:37 +0800

    完善界面，添加元件库

--- Project1/ElementDraw.cpp
+++ Project1/ElementDraw.cpp
@@ -9,29 +9,47 @@ void DrawElement(wxDC& dc, const std::string& type, const std::string& color, in
 
     if (type == "AND") {
         dc.DrawRectangle(x, y, 30, 40);
-        dc.DrawArc(x+30, y+40, x+30, y, x+30, y+20);
-        dc.DrawLine(x-10, y+10, x, y+10);
-        dc.DrawLine(x-10, y+30, x, y+30);
-        dc.DrawLine(x+50, y+20, x+60, y+20);
-        }
-        else if (type == "OR") {
+        dc.DrawArc(x + 30, y + 40, x + 30, y, x + 30, y + 20);
+        dc.DrawLine(x - 10, y + 10, x, y + 10);
+        dc.DrawLine(x - 10, y + 30, x, y + 30);
+        dc.DrawLine(x + 50, y + 20, x + 60, y + 20);
+    }
+    else if (type == "OR") {
         // OR门：左弧线+右大弧线
-        dc.DrawArc(x+20, y+40, x+20, y, x+30, y+20); // 右大弧
-        dc.DrawArc(x+20, y+40, x+20, y, x+5, y+20); // 左小弧
-        dc.DrawLine(x+10, y+10, x+26, y+10);
-        dc.DrawLine(x+10, y+30, x+26, y+30);
-        dc.DrawLine(x+50, y+20, x+60, y+20);
-        }
-        else if (type == "NOT") {
+        dc.DrawArc(x + 20, y + 40, x + 20, y, x + 30, y + 20); // 右大弧
+        dc.DrawArc(x + 20, y + 40, x + 20, y, x + 5, y + 20); // 左小弧
+        dc.DrawLine(x + 10, y + 10, x + 26, y + 10);
+        dc.DrawLine(x + 10, y + 30, x + 26, y + 30);
+        dc.DrawLine(x + 50, y + 20, x + 60, y + 20);
+    }
+    else if (type == "NOT") {
         // NOT门：三角形+小圆
         wxPoint points[3] = {
             wxPoint(x, y),
-            wxPoint(x, y+40),
-            wxPoint(x+40, y+20)
+            wxPoint(x, y + 40),
+            wxPoint(x + 40, y + 20)
         };
         dc.DrawPolygon(3, points);
-        dc.DrawCircle(x+45, y+20, 5); 
-        dc.DrawLine(x-10, y+20, x, y+20);
-        dc.DrawLine(x+50, y+20, x+60, y+20);
+        dc.DrawCircle(x + 45, y + 20, 5);
+        dc.DrawLine(x - 10, y + 20, x, y + 20);
+        dc.DrawLine(x + 50, y + 20, x + 60, y + 20);
+    }
+    else if (type == "XNOR") {
+        //XOR门：左弧线+三角形
+        dc.DrawArc(x + 10, y + 40, x + 10, y, x , y + 20);
+        dc.DrawArc(x + 20, y + 40, x + 20, y, x + 30, y + 20); // 右大弧
+        dc.DrawArc(x + 20, y + 40, x + 20, y, x + 5, y + 20); // 左小弧
+        dc.DrawCircle(x + 55, y + 20, 5);
+        dc.DrawLine(x , y + 10, x + 17, y + 10);
+        dc.DrawLine(x , y + 30, x + 17, y + 30);
+        dc.DrawLine(x + 60, y + 20, x + 70, y + 20);
+    }
+    else if (type == "XOR") {
+        dc.DrawArc(x + 10, y + 40, x + 10, y, x , y + 20);
+        dc.DrawArc(x + 20, y + 40, x + 20, y, x + 30, y + 20); // 右大弧
+        dc.DrawArc(x + 20, y + 40, x + 20, y, x + 5, y + 20); // 左小弧
+        dc.DrawLine(x, y + 10, x + 17, y + 10);
+        dc.DrawLine(x, y + 30, x + 17, y + 30);
+        dc.DrawLine(x + 50, y + 20, x + 60, y + 20);
     }
 }
