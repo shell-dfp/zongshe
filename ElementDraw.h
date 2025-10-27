5114e13a96dcdbbbabc1ddb5d45c3204e919cf7f
shell-dfp <1574138381@qq.com>
2025-10-27 17:37:44 +0800

    Add GetElementPins function and include nlohmann/json

--- ElementDraw.h
+++ ElementDraw.h
@@ -1,5 +1,9 @@
 #pragma once
-#include<wx/dc.h>
-#include<string>
+#include <wx/dc.h>
+#include <wx/panel.h>
+#include <vector>
+#include <string>
+#include <nlohmann/json.hpp>
 
 void DrawElement(wxDC& dc, const std::string& type, const std::string& color, int thickness, int x, int y);
+std::vector<wxPoint> GetElementPins(const nlohmann::json& el);
